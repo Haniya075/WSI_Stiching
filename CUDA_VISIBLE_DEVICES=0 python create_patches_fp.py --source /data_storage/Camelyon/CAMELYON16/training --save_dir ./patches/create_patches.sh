55db3cc1a3a64734214ac14d62b2827c4d553848
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-05 11:49:56 +0500

    Create create_patches.sh

---
+++ CUDA_VISIBLE_DEVICES=0 python create_patches_fp.py --source /data_storage/Camelyon/CAMELYON16/training --save_dir ./patches/create_patches.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python create_patches_fp.py --source /kaggle/input/wsi-images/WSI --save_dir ./patches/Camelyon16_patch256_ostu --patch_level 1 --patch_size 256 --step_size 256 --seg --patch --stitch --use_ostu
