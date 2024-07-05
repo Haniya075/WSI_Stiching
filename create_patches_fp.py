2bd9c838dff92763b625a4aa14db8e3274c44880
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-05 15:22:45 +0500

    Update create_patches_fp.py

--- create_patches_fp.py
+++ create_patches_fp.py
@@ -156,7 +156,7 @@ def seg_and_patch(source, save_dir, patch_save_dir, mask_save_dir, stitch_save_d
 				current_vis_params['vis_level'] = 0
 			else:
 				wsi = WSI_object.getOpenSlide()
-				best_level = wsi.get_best_level_for_downsample(64)
+				best_level = wsi.get_best_level_for_downsample(5)
 				current_vis_params['vis_level'] = best_level
 
 		if current_seg_params['seg_level'] < 0:
@@ -165,7 +165,7 @@ def seg_and_patch(source, save_dir, patch_save_dir, mask_save_dir, stitch_save_d
 			
 			else:
 				wsi = WSI_object.getOpenSlide()
-				best_level = wsi.get_best_level_for_downsample(64)
+				best_level = wsi.get_best_level_for_downsample(5)
 				current_seg_params['seg_level'] = best_level
 
 		keep_ids = str(current_seg_params['keep_ids'])
