0e63e2a68a86ed802d0e6f22df83c912fe55a787
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-07 12:32:52 +0500

    Update wsi_utils.py

--- wsi_core/wsi_utils.py
+++ wsi_core/wsi_utils.py
@@ -66,7 +66,7 @@ def StitchPatches(hdf5_file_path, downscale=5, draw_grid=False, bg_color=(0,0,0)
     file.close()
     return heatmap
 
-def StitchCoords(hdf5_file_path, wsi_object, downscale=16, draw_grid=False, bg_color=(0,0,0), alpha=-1):
+def StitchCoords(hdf5_file_path, wsi_object, downscale=5, draw_grid=False, bg_color=(0,0,0), alpha=-1):
     wsi = wsi_object.getOpenSlide()
     vis_level = wsi.get_best_level_for_downsample(downscale)
     file = h5py.File(hdf5_file_path, 'r')
