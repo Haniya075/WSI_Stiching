17b1c21cfafe13510fdbcfadf997ae769c2a3b14
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-08 18:59:03 +0500

    Update create_patches_fp.py

--- create_patches_fp.py
+++ create_patches_fp.py
@@ -14,6 +14,7 @@ import pandas as pd
 
 def stitching(file_path, wsi_object, downscale = 5):
 	start = time.time()
+	print("DOWNSCALE : ",downscale)
 	heatmap = StitchCoords(file_path, wsi_object, downscale=downscale, bg_color=(0,0,0), alpha=-1, draw_grid=False)
 	total_time = time.time() - start
 	
