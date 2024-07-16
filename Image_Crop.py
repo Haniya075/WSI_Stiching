236780c3d9e49118f89e285df92da1f2e1fb6c7f
Haniya075 <129097930+Haniya075@users.noreply.github.com>
2024-07-16 14:09:53 +0500

    Update Image_Crop.py

--- Image_Crop.py
+++ Image_Crop.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 
 def crop_image(image, output_path):
-    #image = np.array(image)
+    image = np.array(image)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     _, thresh = cv2.threshold(gray, 1, 255, cv2.THRESH_BINARY)
     contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
